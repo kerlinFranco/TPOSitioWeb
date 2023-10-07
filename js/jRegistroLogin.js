82980ba2f4670298e6400ba6646088e6eec47114
kerlinFranco <kerlinfranco@gmail.com>
2023-10-07 15:31:14 -0300

    registro

---
+++ js/jRegistroLogin.js
@@ -0,0 +1,60 @@
+document.getElementById("bRegistrar").addEventListener("click",registro);
+document.getElementById("bIniciarSession").addEventListener("click",login);
+window.addEventListener("resize",anchoPag);
+
+
+let contenedorLoginRegistro=document.querySelector(".contenedorLoginRegistro");
+let formularioLogin=document.querySelector(".formularioLogin");
+let formularioRegistro=document.querySelector(".formularioRegistro");
+let cajaAtrasLogin=document.querySelector(".cajaLogin");
+let cajaAtrasRegistro=document.querySelector(".cajaRegistro");
+
+function anchoPag(){
+    if(window.innerWidth>850){
+        cajaAtrasLogin.style.display="block";
+        cajaAtrasRegistro.style.display="block";
+    }else{
+        cajaAtrasRegistro.style.display="block";
+        cajaAtrasRegistro.style.opacity="1";
+        cajaAtrasLogin.style.display="none";
+        formularioLogin.style.display="block";
+        formularioRegistro.style.display="none";
+        contenedorLoginRegistro.style.left="0px";
+    }
+}
+
+anchoPag();
+
+function registro(){
+    if(window.innerWidth>850){
+        formularioRegistro.style.display="block";
+        contenedorLoginRegistro.style.left="460px";
+        formularioLogin.style.display="none";
+        cajaAtrasRegistro.style.opacity="0";
+        cajaAtrasLogin.style.opacity="1";
+    }else{
+        formularioRegistro.style.display="block";
+        contenedorLoginRegistro.style.left="0px";
+        formularioLogin.style.display="none";
+        cajaAtrasRegistro.style.display="none";
+        cajaAtrasLogin.style.display="block";
+        cajaAtrasLogin.style.opacity="1";
+    }
+    
+}
+
+function login(){
+    if(window.innerWidth>850){
+        formularioRegistro.style.display="none";
+        contenedorLoginRegistro.style.left="10px";
+        formularioLogin.style.display="block";
+        cajaAtrasRegistro.style.opacity="1";
+        cajaAtrasLogin.style.opacity="0";
+    }else{
+        formularioRegistro.style.display="none";
+        contenedorLoginRegistro.style.left="0px";
+        formularioLogin.style.display="block";
+        cajaAtrasRegistro.style.display="block";
+        cajaAtrasLogin.style.display="none";  
+    }
+}
