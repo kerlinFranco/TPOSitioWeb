7b076fa414e175896cea65db4981e3bb6525cae8
kerlinFranco <kerlinfranco@gmail.com>
2023-10-08 09:04:31 -0300

    modificaciones del form

---
+++ js/validarRegistroLogin.js
@@ -0,0 +1,7 @@
+//Login
+let email=document.getElementById("emailLogin");
+let passL=document.getElementById("passwordLogin");
+
+// form.addEventListener("submit",e=>){
+//     if(email.maches(""))
+// }
