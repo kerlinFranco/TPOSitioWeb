5ce671b6c78c46f24beb2dc97e50a1ab777de2e6
realtanatos <fabianargento@gmail.com>
2023-10-09 16:08:02 -0300

    Index v1
    Index v1, estructura de grillas

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let cad=`
+<h1>Saraza Winery Logo y Banner</h1>
+        <nav>
+            <a class="navheader" href="index.html">Inicio</a>
+            <a class="navheader" href="tienda.html">Tienda</a>
+            <a class="navheader" href="cart.html">Carrito de Compras</a>
+            <a class="navheader" href="registerLogin.html">Login</a>
+            <a class="navheader" href="quienesSomos.html">Quienes Somos</a>
+        </nav> `
+
+        document.querySelector("header").innerHTML=cad
+
+
+        cad=`
+        <p>© 2023 Sitio Desarrollado para Codo a Codo por Eduardo Argento - Franco Kerlin - Ximena Melendez - Ivan Reartes</p>
+        `
+
+
+
+document.querySelector("footer").innerHTML=cad
