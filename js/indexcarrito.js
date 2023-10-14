5b5ba45f09325f418306bead97d8e2cde05774dc
Ivan-Reartes <136912293+Ivan-Reartes@users.noreply.github.com>
2023-10-13 22:42:35 -0300

    Carrito Y Tienda 1.0.1

---
+++ js/indexcarrito.js
@@ -0,0 +1,17 @@
+const contenedorTarjetas = document.getElementById("productos-container");
+
+
+function crearTarjetasProductosInicio(productos){
+  productos.forEach(producto => {
+    const nuevoVino = document.createElement("div");
+    nuevoVino.classList = "tarjeta-producto"
+    nuevoVino.innerHTML = `
+    <img src="./imagenes/vinos/malbec/${producto.id}.jpg" alt="Vinos">
+    <h3>${producto.nombre}</h3>
+    <p class="precio">$${producto.precio}</p>
+    <button>Agregar al carrito</button>`
+    contenedorTarjetas.appendChild(nuevoVino);
+    nuevoVino.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+  });
+}
+crearTarjetasProductosInicio(vinosMalbec);
---
+++ js/vinos.js
@@ -0,0 +1,37 @@
+const vinosMalbec = [
+    {
+      id:1,
+      nombre: "A Lisa - Malbec",
+      precio: 4500,
+    },
+    {
+      id:2,
+      nombre: "El Gran Enemigo - Malbec",
+      precio: 12599,
+    },
+    {
+      id:3,
+      nombre: "Enzo Bienachi - Gran Malbec",
+      precio: 15399,
+    },
+    {
+      id:4,
+      nombre: "Pulenta - Gran Malbec",
+      precio: 8999,
+    },
+    {
+      id:5,
+      nombre: "Salentein Reserve - Malbec",
+      precio: 7999,
+    },
+    {
+      id:6,
+      nombre: "Terraza de los Andes - Malbec",
+      precio: 16999,
+    },
+    {
+      id:7,
+      nombre: "Old Vineyard Humberto Canale - Malbec",
+      precio: 24999,
+    }
+  ]
