b840ed646072a5d04f2b9e78b0a5bbbf0d167995
kerlinFranco <kerlinfranco@gmail.com>
2023-10-21 00:34:49 -0300

    se agrego 21 vinos al json
    a partir del vino 21 no hay mas imagenes

--- js/jRegistroLogin.js
+++ js/jRegistroLogin.js
@@ -54,10 +54,10 @@ fetch('vinosLocal.json')
         .then(response => response.json())
         .then(data => {
             const ofertasSidebar = document.getElementById('ofertas-sidebar');
-            let num=parseInt((Math.random()*100)+1)
-            console.log(num)
+            let num=parseInt((Math.random()*21)+1)
+            
             data.forEach(oferta => {
-                if(oferta.id==1){
+                if(oferta.id==num){
                     const ofertaCard = document.createElement('div');
                 ofertaCard.classList.add('oferta-card');
                 ofertaCard.innerHTML = `
@@ -73,6 +73,27 @@ fetch('vinosLocal.json')
                 }
                 
             });
+            const ofertasSidebar2 = document.getElementById('ofertas-sidebar2');
+            let num2=parseInt((Math.random()*21)+1)
+            
+            data.forEach(oferta => {
+                if(oferta.id==num2){
+                    const ofertaCard2 = document.createElement('div');
+                ofertaCard2.classList.add('oferta-card2');
+                ofertaCard2.innerHTML = `
+                    <h3>${oferta.nombre}</h3>
+                    <img src="${oferta.fotoTarjeta}" alt="Bienvenido a Sara Winery Club"
+                    class="widget">
+                    <p>${oferta.descripcion}</p>
+                    <h2>${oferta.precioClub}</h2>
+                    <a href="${oferta.enlace}" target="blank">Ver oferta</a>
+                    
+                `;
+                ofertasSidebar2.appendChild(ofertaCard2);
+                }
+                
+            });
+
         })
         .catch(error => {
             console.error('Error al cargar las ofertas: ', error);
