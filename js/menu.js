ee5b1ba0670dbe991369308eccef34a41b344970
realtanatos <fabianargento@gmail.com>
2023-10-24 13:51:07 -0300

    Muchos cambios
    Van al INDEX!

--- js/menu.js
+++ js/menu.js
@@ -20,3 +20,20 @@ cad = `
 
 
 document.querySelector("footer").innerHTML = cad
+
+
+document.addEventListener("DOMContentLoaded", function () {
+        new Vue({
+          el: "#randomHeroImage",
+          data: {
+            imagenes: [
+                "../imagenes/heroImage1.png",
+                "../imagenes/heroImage2.png",
+                "../imagenes/heroImage3.png",
+                "../imagenes/heroImage4.png"
+            ],
+            imagenSeleccionada: ""
+          }
+        });
+      });
+      
