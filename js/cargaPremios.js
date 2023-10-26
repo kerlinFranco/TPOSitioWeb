a1ff47733903ca3582a1926372e4c54cfa3941cb
realtanatos <fabianargento@gmail.com>
2023-10-26 10:48:53 -0300

    cambios al json de premios
    y mejoras cosmeticas

--- js/cargaPremios.js
+++ js/cargaPremios.js
@@ -1,84 +1,82 @@
-var app = new Vue({
-    el: '#cargaPremios',
-    data: {
-        registros: [],
-        registrosLocalStorage: {},
-        registro1: null,
-        registro2: null,
-        registro3: null,
-    },
-    methods: {
-        // Cargar datos del JSON
-        cargarJson: function () {
-            var jsonURL = 'https://saraza-wine-club.netlify.app/json/premios.json';
-            console.log("A leer el JSON de premios");
-            // Paso 1: Intentamos cargar los datos del JSON
-            fetch(jsonURL)
-                .then(response => response.json())
-                .then(data => {
-                    console.log("Datos de premios cargados con éxito");
-                    console.log("Tenemos una banda de premios: " + data.length);
-
-                    if (data.length > 0) {
-                        this.registros = data;
-                        console.log("Se guardaron los datos del JSON en nuestra memoria");
-                        // Paso 2: Guardar los datos en nuestro almacén local
-                        this.guardarEnLocalStorage();
-                    } else {
-                        console.log("Error - No hay premios en la lista.");
-                    }
-                })
-                .catch(error => {
-                    console.error('Algo salió mal al cargar los datos del JSON:', error);
-                });
-        },
-            // Guardar datos en nuestro almacén local si no existen ya
-            guardarEnLocalStorage: function () {
-                if (this.registros.length > 0 && !localStorage.getItem('premiosRegistro')) {
-                    localStorage.setItem('premiosRegistro', JSON.stringify(this.registros));
-                    console.log('Ahi ya guardamos los datos JSON de los premios en nuestro localStorage');
-                } else if (localStorage.getItem('premiosRegistro')) {
-                    console.log('Ya tenemos datos en nuestro localStorage. No necesitamos sobrescribirlos.');
-                } else {
-                    console.log('No hay datos para guardar en nuestro localStorage');
-                }
-            },
-        // Cargar datos de nuestra bodega local
-        cargarLocalStorage: function () {
-            var registros = JSON.parse(localStorage.getItem('premiosRegistro'));
-            if (registros) {
-                this.registrosLocalStorage = registros;
-                console.log('Cargamos bien los datos de vinos desde nuestro localStorage');
-            } else {
-                console.log('No hay datos de vinos en nuestro localStorage para cargar. Volvamos al paso 1 por si fallo algo.');
-
-                // Paso 1: Intentemos cargar el JSON nuevamente
-                this.cargarJson();
-            }
-        },
-        // Elegir dos vinos al azar y mostrarlos
-        elegirRegistrosAlAzar: function () {
-            if (Object.keys(this.registrosLocalStorage).length > 0) {
-                var keys = Object.keys(this.registrosLocalStorage);
-                var indice1 = Math.floor(Math.random() * keys.length);
-                var indice2 = Math.floor(Math.random() * keys.length);
-                var indice2 = Math.floor(Math.random() * keys.length);
-
-                this.registro1 = this.registrosLocalStorage[keys[indice1]];
-                this.registro2 = this.registrosLocalStorage[keys[indice2]];
-                this.registro2 = this.registrosLocalStorage[keys[indice2]];
-
-                console.log('Elegimos tres premios al azar para mostrar en PREMIOS Y DISTINCIONES');
-                console.log('Premio: ' + this.registro1);
-                console.log('Premio: ' + this.registro2);
-                console.log('Premio: ' + this.registro3);
-            } else {
-                console.log('No hay datos de premios en nuestro localStorage para elegir al azar. Debe haber fallado el JSON.');
-            }
+        // Función para obtener un número aleatorio entre un rango
+        function getRandomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
         }
-    },
-});
 
-// Ejecutamos las funciones necesarias
-app.cargarLocalStorage();
-app.elegirRegistrosAlAzar();
+        // Función para cargar un archivo JSON
+        function loadJSON(callback) {
+            const xhr = new XMLHttpRequest();
+            xhr.overrideMimeType("application/json");
+            xhr.open("GET", "premios.json", true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    callback(xhr.responseText);
+                }
+            };
+            xhr.send(null);
+        }
+
+// Función para verificar si un elemento está en la vista del usuario
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+/* un detalle cosmetico */
+
+// Activar la animación cuando se vea la tarjeta
+function animateOnScroll() {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card) => {
+        if (isElementInViewport(card)) {
+            card.classList.add("animate__animated", "animate__fadeIn"); // Agrega clases de animación
+        }
+    });
+}
+
+// Solo ejecuta la función al cargar la página y cuando se hace scroll asi el usuario ve la animacion cuando llega
+document.addEventListener("DOMContentLoaded", animateOnScroll);
+window.addEventListener("scroll", animateOnScroll);
+
+
+        // Generar tarjetas
+        function generateCards(data) {
+            const cardContainer = document.getElementById("contenedor-tarjeta");
+            const jsonData = JSON.parse(data);
+
+            // Obtener 3 índices random
+            const randomIndexes = [];
+            while (randomIndexes.length < 3) {
+                const randomIndex = getRandomNumber(0, jsonData.length - 1);
+                if (!randomIndexes.includes(randomIndex)) {
+                    randomIndexes.push(randomIndex);
+                }
+            }
+
+            // Crear las tarjetas con los 3 registros
+            randomIndexes.forEach((index) => {
+                const premio = jsonData[index];
+                const card = document.createElement("div");
+                card.classList.add("tarjeta");
+                card.innerHTML = `
+                    <h2>${premio.nombre}</h2>
+                    <p>Asociación: ${premio.asociacion}</p>
+                    <p>Cargo Directivo: ${premio.cargo_directivo}</p>
+                    <p>Nombre del Premio: ${premio.nombre_premio}</p>
+                    <p>Valoración: ${premio.valoracion}</p>
+                    <img src="${premio.imagen_persona}" alt="${premio.nombre}">
+                    <img src="${premio.imagen_premio}" alt="${premio.nombre_premio}">
+                `;
+                cardContainer.appendChild(card);
+            });
+        }
+
+        // Cargar el archivo JSON y generar tarjetas
+        loadJSON(generateCards);
+        
